ee722af9887f1dcbf56747911390866472f33065
Javier Gutierrez <jjavierweb@users.noreply.github.com>
2018-08-24 20:25:51 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+{
+  "workbench.colorTheme": "One Dark Pro",
+  "files.autoSave": "onFocusChange",
+  "editor.minimap.enabled": true,
+  "workbench.statusBar.visible": true,
+  "workbench.activityBar.visible": true,
+  "editor.formatOnSave": false,
+  "prettier.singleQuote": true,
+  "workbench.colorCustomizations": {
+    "statusBar.background": "#333333",
+    "statusBar.noFolderBackground": "#333333",
+    "statusBar.debuggingBackground": "#263238"
+  },
+  "workbench.iconTheme": "vscode-icons",
+  "css.validate": false,
+  "scss.validate": false,
+  "less.validate": false,
+  "editor.wordWrap": "on",
+  "explorer.confirmDelete": false
+}
