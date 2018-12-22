03ba5eb2e3b38f20f4a8def029688e6fa317a4bc
Javier Gutierrez <jjavierweb@users.noreply.github.com>
2018-12-22 00:18:48 -0600

    Update config.js
    Added new version of the config file for the vscode

--- config.js
+++ config.js
@@ -22,3 +22,41 @@
   "workbench.iconTheme": "vscode-icons",
   "html-css-class-completion.enableEmmetSupport": true
 }
+
+// new version
+{   
+    //this will disable reports to microsoft
+    "telemetry.enableTelemetry": false,
+    "telemetry.enableCrashReporter": false,
+
+    //set vscode icons and other workbench settings
+    "workbench.iconTheme": "vscode-icons",
+    "workbench.activityBar.visible": true,
+    "workbench.statusBar.visible": true,
+
+    //editor settings
+    "editor.formatOnSave": false,
+    "editor.minimap.enabled": true,
+    "editor.wordWrap": "on",
+
+    //pretier settings
+    "prettier.singleQuote": true,
+
+    //emmet settings
+    "emmet.includeLanguages": {
+        "javascript": "javascript"
+    },
+
+    //files settings
+    "files.autoSave": "onFocusChange",
+    "explorer.confirmDelete": false,
+
+    //live server settings
+    "liveServer.settings.donotShowInfoMsg": true,
+
+    //javascript specific settings
+    "javascript.autoClosingTags": true,
+    "javascript.updateImportsOnFileMove.enabled": "always",
+
+
+}
