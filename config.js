2cb12fdb9b7eae3126813c66e1ef35c33e379d4c
Javier Gutierrez <jjavierweb@users.noreply.github.com>
2021-02-18 21:05:16 -0600

    Update config.js
    Even newer version

--- config.js
+++ config.js
@@ -1,40 +1,89 @@
-
-// new version
-{   
-    //this will disable reports to microsoft
+{
+    // Disable sending telemetry to microsoft
     "telemetry.enableTelemetry": false,
     "telemetry.enableCrashReporter": false,
 
-    //set vscode icons and other workbench settings
-    "workbench.iconTheme": "vscode-icons",
+    // Set vscode icons and benchmark settings
     "workbench.activityBar.visible": true,
     "workbench.statusBar.visible": true,
 
-    //editor settings
+    // Editor settings
     "editor.formatOnSave": false,
     "editor.minimap.enabled": true,
     "editor.wordWrap": "on",
+    "editor.lineNumbers": "on",
+    "editor.linkedEditing": true,
+    "editor.autoIndent": "advanced",
+    "editor.fontLigatures": true,
 
-    //pretier settings
+    // Prettier settings
     "prettier.singleQuote": true,
+    "prettier.enable": true,
+    "prettier.tabWidth": 2,
 
-    //emmet settings
-    "emmet.includeLanguages": {
-        "javascript": "javascript"
-    },
-
-    //files settings
+    // Emmet settings
+    "emmet.excludeLanguages": [
+        "markdown"
+    ],
+    
+    // File settings
     "files.autoSave": "onFocusChange",
     "explorer.confirmDelete": false,
+    "files.associations": {
+        "*.html": "html",
+        "*.txt": "txt",
+    },
 
-    //live server settings
+    // Live server settings
     "liveServer.settings.donotShowInfoMsg": true,
 
-    //javascript specific settings
+    // bracket pair colorizer
+    "bracket-pair-colorizer-2.forceUniqueOpeningColor": true,
+    "editor.tabSize": 2,
+
+    // Workbench settings
+    "workbench.colorTheme": "GitHub Dark",
+    "workbench.iconTheme": "material-icon-theme",
+    "workbench.editor.decorations.badges": true,
+    "workbench.colorCustomizations": {
+        "activityBarBadge.background": "#2CA142",
+        "badge.background": "#2ca142",
+        "badge.foreground": "#fff",
+        //"activityBar.inactiveForeground": "#2ca000",
+        "activityBar.foreground": "#2ca000",
+        //"activityBar.activeBorder": "#eee",
+        
+
+    },
+    
+
+    // Code spaces
+    "codespaces.defaultExtensions": [
+        "ms-vsonline.vsonline",
+        "GitHub.vscode-pull-request-github",
+        "ms-vsliveshare.vsliveshare",
+        "visualstudioexptteam.vscodeintellicode"
+      ],
+    "codespaces.accountProvider": "Uninitialized",
+    
+    // Html and CSS settings
+    "css.validate": false,
+    "scss.validate": false,
+    "less.validate": false,
+
+    // Markdown settings
+    "markdown-preview-enhanced.liveUpdate": true,
+
+    // javascript specific settings
     "javascript.autoClosingTags": true,
     "javascript.updateImportsOnFileMove.enabled": "always",
-  
-    //html and css settings
-    "html-css-class-completion.enableEmmetSupport": true,
+
+    // Python and django
+    "emmet.includeLanguages": {"djnago-html": "html"},
+    "python.linting.pylintEnabled": true,
+    "python.linting.enabled": true,
+    "vsicons.associations.files": [
+        {"**/templates/**/*.html": "django-html"},
+    ],
 
 }
